3ab4533c5506ad967c5306f4585bce87da07a874
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2021-10-22 17:50:44 -0300

    feat: add project files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
-      <h1>AYOOOOOOOO</h1>
+      <Navbar />
+      <Hero />
     </div>
   );
 }

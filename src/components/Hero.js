1bf032a51084662eb1d42e5bec3f59320e664530
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2021-10-22 20:21:06 -0300

    feat: update hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,84 @@
 import React from "react";
+import styled from "styled-components";
+import BgImg from "../assets/bg-image.png";
+
+const Section = styled.section`
+  height: 785px;
+  display: block;
+  background-repeat: no-repeat;
+  background-size: contain;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+`;
+
+const Left = styled.div`
+  padding-left: 220px;
+  padding-top: 143px;
+`;
+
+const Right = styled.div`
+  width: 676px;
+  height: 100%;
+`;
+
+const Title = styled.p`
+  font-size: 55px;
+  color: #04050a;
+  font-weight: 400;
+`;
+
+const Desc = styled.p`
+  width: 472px;
+  font-size: 20px;
+  color: #9ea0ac;
+  line-height: 30px;
+  margin-top: 58px;
+`;
+
+const Button = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 18px;
+  margin-top: 58px;
+  width: 371px;
+  height: 71px;
+  line-height: 71px;
+  font-size: 22px;
+  text-align: center;
+  color: #fff;
+  cursor: pointer;
+  background: linear-gradient(90deg, #0546d6, #3f89fc);
+  text-decoration: none;
+  box-shadow: 0 15px 14px rgb(0 42 177 / 12%);
+`;
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero Section</h1>
-    </div>
+    <Section>
+      <Content>
+        <Left>
+          <Title>
+            Get 2 FREE Stocks <br /> valued up to $1850!
+          </Title>
+          <Desc>
+            Open and fund a brokerage account with $100 or more and you will
+            have a chance of claiming stocks like <span> GOOG, FB, SBUX</span>{" "}
+            and more!
+          </Desc>
+          <Button href="https://bit.ly/webull-join" target="_blank">
+            <span>Claim your free stocks now</span>
+          </Button>
+        </Left>
+        <Right>
+          <img alt="hero" src={BgImg}></img>
+        </Right>
+      </Content>
+    </Section>
   );
 };
 
